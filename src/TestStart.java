1856c6dacd1737c1df57d000d527eb9eebfe56c2
lindsirje <lindsirje@gmail.com>
2017-03-20 19:44:38 +0200

    tegin testfaili

---
+++ src/TestStart.java
@@ -0,0 +1,8 @@
+/**
+ * Created by lindsirj on 20.03.2017.
+ */
+public class TestStart {
+    public static void main (String [] args){
+
+    }
+}
--- src/koostaKontrolltöö.java
+++ src/koostaKontrolltöö.java
@@ -2,4 +2,9 @@
  * Created by kersti on 20.03.17.
  */
 public class koostaKontrolltöö {
+    String nimi;
+
+    public koostaKontrolltöö(String nimi) {
+        this.nimi = nimi;
+    }
 }
