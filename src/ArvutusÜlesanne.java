b6148551d01a813dace623cc7eba151a61ca96a5
kivisook <kersti.kivisoo@mail.ee>
2017-04-01 21:09:18 +0300

    Nüüd tagastab Ülesande tüübi

--- src/ArvutusÜlesanne.java
+++ src/ArvutusÜlesanne.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
  * Created by kersti on 01.04.17.
  */
 public class ArvutusÜlesanne extends Ülesanne {
-    private String ID;
-    private String küsimus;
-    private String vastus;
 
     //arvutusülesande failis on igas reas: jrk.nr, tühik, ülesanne, tühik, vastus
 
@@ -14,29 +11,25 @@ public class ArvutusÜlesanne extends Ülesanne {
         super(ID, küsimus, vastus);
     }
 
-    public static void tükeldaÜlesanne (ArrayList YlList, ArrayList VaList, String rida) {
-        String[] tükid = rida.split(" ");   //osad on eraldatud tõhikutega
+
+    public static Ülesanne tükeldaÜlesanne (String rida) {
+
+        String[] tükid = rida.split(" ");   //osad on eraldatud tühikutega
+        String indeks = tükid[0];                   //jrk.nr on indeksiks
         String ülesandetekst = tükid[1];            //ülesanded on teisel kohal
         String vastus = tükid[2];                   //vastused on kolmandal kohal
 
-        trükiÜlesanne(YlList, ülesandetekst);   //lisab ülesande teksti etteantud faili
-        trükiVastus(VaList, vastus);            //lisab vastuse etteantud faili
+        Ülesanne yl = new Ülesanne(indeks, ülesandetekst, vastus);
+        return yl;
     }
 
-    public static ArrayList  trükiÜlesanne(ArrayList Ylesanded1, String üksülesanne) { //kirjutab etteantud ülesannetefaili ühe ülesande juurde
-        //System.out.println(üksülesanne);
-        Ylesanded1.add(üksülesanne);
-        return Ylesanded1;
+    public static void  trükiÜlesanne(String üksülesanne) { //trükib ülesande teksti
+        System.out.println(üksülesanne);
     }
 
-    public static ArrayList trükiVastus(ArrayList Vastused1, String üksvastus) {  //kirjutab vastustefaili ühe vastuse juurde
-        Vastused1.add(üksvastus);
-        return Vastused1;
+    public static void trükiVastus(String üksvastus) {  //kirjutab vastustefaili ühe vastuse juurde
+        System.out.println(üksvastus);
     }
 
-    @Override
-    public String toString() {
-        return getKüsimus() + " " + getVastus();
-    }
 }
 
--- src/TekstÜlesanne.java
+++ src/TekstÜlesanne.java
@@ -4,31 +4,34 @@ import java.util.ArrayList;
  * Created by kersti on 01.04.17.
  */
 public class TekstÜlesanne extends Ülesanne {
+    private String ID;
+    private String küsimus;
+    private String vastus;
 
-    public TekstÜlesanne(String ID, String küsimus, String vastus) {
-        super(ID, küsimus, vastus);
-    }
-    //tekstülesande failis on ühes reas: ülesande_nimi, tab, ülesande_tekst
-    //teises reas vastuse_nimi, tab, vastuse_tekst
+    public TekstÜlesanne(String ID, String küsimus, String vastus) {super(ID, küsimus, vastus);}
 
-    public static void tükeldaÜlesanne (ArrayList YlList, ArrayList VaList, String ülesanderida, String vastuserida) {
+    //tekstülesande failis on ühes reas: ülesande_id, tab, ülesande_küsimus
+    //teises reas vastuse_id, tab, vastuse_tekst
+
+    public static Ülesanne tükeldaÜlesanne (String ülesanderida, String vastuserida) {
+
         String[] tükid1 = ülesanderida.split("  ");   //osad on eraldatud tab-iga
+        String indeks = tükid1[0];
         String ülesandetekst = tükid1[1];            //ülesanded on esimesel real teisel kohal
 
         String[] tükid2 = vastuserida.split("   ");
         String vastus = tükid2[1];                   //vastused on teisel real teisel kohal
 
-        trükiÜlesanne(YlList, ülesandetekst);   //lisab ülesande teksti etteantud faili
-        trükiVastus(VaList, vastus);            //lisab vastuse etteantud faili
+        Ülesanne yl = new Ülesanne(indeks, ülesandetekst, vastus);
+        return yl;
+
     }
 
-    public static ArrayList  trükiÜlesanne(ArrayList Ylesanded1, String üksülesanne) { //kirjutab etteantud ülesannetefaili ühe ülesande juurde
-        Ylesanded1.add(üksülesanne);
-        return Ylesanded1;
+    public static void  trükiÜlesanne(String üksülesanne) {
+        System.out.println(üksülesanne);
     }
 
-    public static ArrayList trükiVastus(ArrayList Vastused1, String üksvastus) {  //kirjutab vastustefaili ühe vastuse juurde
-        Vastused1.add(üksvastus);
-        return Vastused1;
+    public static void trükiVastus(String üksvastus) {
+        System.out.println(üksvastus);
     }
 }
