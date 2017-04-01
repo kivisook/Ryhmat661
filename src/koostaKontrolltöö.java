1d45e6d08e08f234c6d5207b405f9ff838f9b836
kivisook <kersti.kivisoo@mail.ee>
2017-04-01 11:03:59 +0300

    Arvutusülesannete klass (kuskil on viga sees, sest näitab hüüumärki)

--- src/koostaKontrolltöö.java
+++ src/koostaKontrolltöö.java
@@ -1,10 +1,14 @@
+import java.util.ArrayList;
+
 /**
  * Created by kersti on 20.03.17.
  */
 public class koostaKontrolltöö {
-    String nimi;
+    private String pealkiri;
+    private ArrayList<Ülesanne> ülesanded;
 
-    public koostaKontrolltöö(String nimi) {
-        this.nimi = nimi;
+    public koostaKontrolltöö(String nimi, ArrayList<Ülesanne> ülesanded) {
+        this.ülesanded = ülesanded;
+        this.pealkiri = pealkiri;
     }
 }
