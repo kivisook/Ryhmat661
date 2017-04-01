dd412c7c334df5f1ab0fb4bdda5b606943d4ee30
kivisook <kersti.kivisoo@mail.ee>
2017-04-01 11:28:17 +0300

    Arvutusülesannete klass: katsetus, kas loeb ülesande teksti ühte listi ja vastused teise listi.

--- src/TestStart.java
+++ src/TestStart.java
@@ -1,8 +1,21 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by lindsirj on 20.03.2017.
  */
 public class TestStart {
-    public static void main (String [] args){
+    public static void main (String [] args) throws FileNotFoundException {
+        File file = new File("ylesanded.txt");
+        Scanner scanner = new Scanner(file, "UTF-8");
+        while (scanner.hasNextLine()) {
+            String rida = scanner.nextLine();
+            System.out.println(ArvutusÜlesanne.tükeldaÜlesanne(rida)); //katsetus, et meetod töötab
+        }
+        scanner.close();
+
 
     }
 }
