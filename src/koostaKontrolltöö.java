e24bc2e65951b931bf449ba7c824506bade7cb2e
kivisook <kersti.kivisoo@mail.ee>
2017-04-03 20:50:48 +0300

    Update koostaKontrolltöö.java
    kustutatud lisaFailistÜlesanded ja ümber nimetatud lisaFailistÜlesanded1 -> lisaFailistÜlesanded

--- src/koostaKontrolltöö.java
+++ src/koostaKontrolltöö.java
@@ -20,37 +20,7 @@ public class koostaKontrolltöö {
         this.pealkiri = pealkiri;
     }
 
-    public static ArrayList<Ülesanne> lisaFailistÜlesanded(File sisendfail, String tüüp) throws FileNotFoundException {
-
-        ArrayList<Ülesanne> ülesanneteList = new ArrayList<>();
-
-        Scanner scanner = new Scanner(sisendfail, "UTF-8");
-                //kui etteantud tüüp on arvutusülesanne
-        if (tüüp.equals("arvutusülesanne")) {
-            while (scanner.hasNextLine()) {
-                String rida = scanner.nextLine();
-                ülesanneteList.add(ArvutusÜlesanne.tükeldaÜlesanne(rida)); //rakendab arvutusülesannete klassi meetodid
-            }
-        }
-            //kui etteantud tüüp on tekstülesanne
-        else if (tüüp.equals("tekstülesanne")) {
-            ArrayList<String> YlVa = new ArrayList<>();
-            while (scanner.hasNextLine()) {
-                String ridadekaupa = scanner.nextLine();
-                YlVa.add(ridadekaupa);
-            }
-            for (int i = 0; i < YlVa.size() - 1; i=i+2) {
-                String ülesanderida = YlVa.get(i);
-                String vastuserida = YlVa.get(i+1);
-                ülesanneteList.add(TekstÜlesanne.tükeldaÜlesanne(ülesanderida, vastuserida)); //rakendab tekstülesannete klassi meetodit
-            }
-        }
-        scanner.close();
-        return ülesanneteList;
-
-    }
-
-    public void lisaFailistÜlesanded1(String failinimi, String tüüp) throws FileNotFoundException {
+    public void lisaFailistÜlesanded(String failinimi, String tüüp) throws FileNotFoundException {
         File file = new File(failinimi);
         Scanner scanner = new Scanner(file, "UTF-8");
 
